c9e655d683e71c2795b33accf6a8d3b9f8641163
respectable2007 <zhanghui8876@163.com>
2018-09-07 16:48:09 +0800

    header

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent implements OnInit {
-
-  constructor() { }
+  
+  userList:any[] = [{
+	  value: '1',
+	  label: '修改密码',
+	}, {
+	  value: '2',
+	  label: '退出登录',
+	}]
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+  
+  handle(e:any): void {
+  	if (e.value === '1') {
 
+  	} else {
+  		this.router.navigate(['login'])
+  	}
+  }
 }
